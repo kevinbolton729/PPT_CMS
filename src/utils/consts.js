21f164c7621028dd63cb590461235729e0ee13c3
kevinbolton729 <kbolton@sohu.com>
2018-04-19 21:53:14 +0800

    edit

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -10,11 +10,11 @@ export const PAGELOGIN = '/user/login';
 export const SECRETKEY_USER = 'SECRETKEY-BOLTON';
 // 接口
 // 开发环境
-export const URL_PREFIX = 'http://127.0.0.1:7001/public';
-export const API_DOMAIN = 'http://127.0.0.1:7001';
+// export const URL_PREFIX = 'http://127.0.0.1:7001/public';
+// export const API_DOMAIN = 'http://127.0.0.1:7001';
 // 生产环境
-// export const URL_PREFIX = 'http://39.108.178.198:7003/public';
-// export const API_DOMAIN = 'http://39.108.178.198:7003';
+export const URL_PREFIX = 'http://39.108.178.198:7003/public';
+export const API_DOMAIN = 'http://39.108.178.198:7003';
 export const LOGO_USER = '../images/logo.png';
 export const LOGO_PAGE = '../images/mainLogo.png';
 // Login/GlobalFooter 提示信息
